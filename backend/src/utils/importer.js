63d733e0d7ffde7c76831e6068460149f9127752
chelsea-banke <chelseanewhorizon@gmail.com>
2023-05-09 11:01:08 +0100

    xls sales files import setup complete

--- backend/src/utils/importer.js
+++ backend/src/utils/importer.js
@@ -1,4 +1,6 @@
 const csvtojson = require('convert-csv-to-json')
+const excel = require('node-xlsx');
+// require('')
 
 const importSales = async (path, source)=>{
     if (source=='legacy'){
@@ -10,10 +12,19 @@ const importSales = async (path, source)=>{
     if (source=='orange'){
         return (JSON.stringify(csvtojson.fieldDelimiter(';').getJsonFromCsv(path), null, 2))
     }
-    if (source=='ecobank'){
-        return (JSON.stringify(csvtojson.fieldDelimiter(';').getJsonFromCsv(path), null, 2))
+    if (source=='ecobank' || source=='afrikpay'){
+        let out = []
+        const sales = (excel.parse(path))[0].data
+        const keys = sales[0]
+        
+        sales.slice(1, -1).forEach(values => {
+            out.push(keys.reduce((obj, key, index) => {
+                obj[key] = values[index];
+                return obj;
+            }, {}))
+        })
+        return(JSON.stringify(out))
     }
-    // else { return (JSON.stringify(csvtojson.getJsonFromCsv(path), null, 2)) }
 }
 
 module.exports = importSales
